2fa1635e3ea1675f4bf5732eae127846b23e4c08
Prideland-Okoi <prideland.okoi@gmail.com>
2022-06-08 23:47:42 +0100

    initial commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse, request
+
+def home_view(request):
+    return render(request, 'home.html')
+
+def about_view(request):
+    return render(request, 'about.html')
+
+def contact_view(request):
+    return render(request, 'contact.html')
